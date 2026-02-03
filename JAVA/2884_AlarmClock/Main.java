d23fb98a9e2352618716bdc494733048f5b2896e
김명철 <audcjf0102@gmail.com>
2026-02-03 12:32:29 +0900

    BOJ_JAVA_2884 solved

---
+++ JAVA/2884_AlarmClock/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        m-=45;
+        if(m<0){
+            m+=60;
+            h--;
+            if(h<0){
+                h=23;
+            }
+        }
+        bw.write(h + " " + m + "\n");
+        bw.flush();
+    }
+}
